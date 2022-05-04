48bb1223d7ef057824c314afa9efbfe4fd5b0ad9
Lassi Piironen <lsipii@kapsi.fi>
2022-05-04 18:10:28 +0300

    fix: scopes

--- app/RuuviGet.py
+++ app/RuuviGet.py
@@ -1,27 +1,31 @@
+from tokenize import Number
 from ruuvitag_sensor.ruuvi import RuuviTagSensor, RunFlag
 
 ###
 # @see: https://github.com/ttu/ruuvitag-sensor
 ###
 class RuuviGet:
+    results: list
+    counter: Number
+    run_flag = None
+
     def execute(self, requestData: dict) -> dict:
 
-        results = []
-        counter = 10
-
+        self.results = []
+        self.counter = 10
         # RunFlag for stopping execution at desired time
-        run_flag = RunFlag()
-
-        def handle_data(found_data):
-            results.append(found_data)
-            counter = counter - 1
-            if counter < 0:
-                run_flag.running = False
+        self.run_flag = RunFlag()
 
         # List of macs of sensors which will execute callback function
         macs = None
 
         # Exec
-        RuuviTagSensor.get_datas(handle_data, macs, run_flag)
+        RuuviTagSensor.get_datas(self.handle_data, macs, self.run_flag)
 
-        return results
+        return self.results
+
+    def handle_data(self, found_data):
+        self.results.append(found_data)
+        self.counter = self.counter - 1
+        if self.counter < 0:
+            self.run_flag.running = False
