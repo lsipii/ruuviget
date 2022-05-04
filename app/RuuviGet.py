f041bbf19ff75022e69e95368af1fe755c8a9c3f
Lassi Piironen <lsipii@kapsi.fi>
2022-05-04 17:48:45 +0300

    feat: ruuvi results

--- app/RuuviGet.py
+++ app/RuuviGet.py
@@ -1,6 +1,27 @@
-from ruuvitag_sensor.ruuvi import RuuviTagSensor
+from ruuvitag_sensor.ruuvi import RuuviTagSensor, RunFlag
 
-
+###
+# @see: https://github.com/ttu/ruuvitag-sensor
+###
 class RuuviGet:
     def execute(self, requestData: dict) -> dict:
-        return RuuviTagSensor.find_ruuvitags()
+
+        results = []
+        counter = 10
+        # RunFlag for stopping execution at desired time
+        run_flag = RunFlag()
+
+        def handle_data(found_data):
+            results.append(found_data)
+            global counter
+            counter = counter - 1
+            if counter < 0:
+                run_flag.running = False
+
+        # List of macs of sensors which will execute callback function
+        macs = None
+
+        # Exec
+        RuuviTagSensor.get_datas(handle_data, macs, run_flag)
+
+        return results
