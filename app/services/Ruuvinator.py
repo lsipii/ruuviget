62904048ed660661e5bf643c839fe15a5d1f860e
Lassi Piironen <lsipii@kapsi.fi>
2022-05-20 14:25:40 +0300

    fix: add simple error handling to the client app

--- app/services/Ruuvinator.py
+++ app/services/Ruuvinator.py
@@ -1,10 +1,16 @@
 from app.services.Requester import Requester
 from app.utils.Settings import Settings
 
-class Ruuvinator():
+
+class Ruuvinator:
     def fetch(self, mac_addresses: list = []) -> list:
-        ruuvi_getator_endpoint_url = Settings().get_setting("RUUVI_GETATOR_SERVICE_URL", "http://localhost:5000/ruuviget")
+        ruuvi_getator_endpoint_url = Settings().get_setting(
+            "RUUVI_GETATOR_SERVICE_URL", "http://localhost:5000/ruuviget"
+        )
         response = Requester().post(url=ruuvi_getator_endpoint_url, data={"mac_addresses": mac_addresses})
+
+        if response["statusCode"] != 200:
+            raise Exception(f"Bad response: {response['reason']}")
 
         if "items" in response and len(response["items"]) > 0:
             return list(map(lambda item: {"mac": item[0], "data": item[1]}, response["items"]))
